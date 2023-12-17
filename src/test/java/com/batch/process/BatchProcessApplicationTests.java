f1323a917fcf46b33296228a43ddf2f940cd8c53
Shweta Singh <goodyshweta@gmail.com>
2023-12-17 12:14:59 -0500

    Spring batch initial setup

---
+++ src/test/java/com/batch/process/BatchProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.process;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
